4404757aea2e37b0ee318a9de7a5b32221bffa3b
NoahDresch <dreschnoah@gmail.com>
2025-09-15 22:25:16 -0300

    aula python 14, dados produtos grafico de pizza

--- aula14/dadosprodutos.py
+++ aula14/dadosprodutos.py
@@ -5,4 +5,9 @@ dados = {
     'qtde_vendida':[33,50,45]
     }
 df = pd.DataFrame(dados)
+
+# Gráfico de barras
 df.plot(x='Produto', y='qtde_vendida', kind='bar')
+
+# Gráfico de pizza
+df.plot(x='Produto', y='qtde_vendida', kind='pie')
