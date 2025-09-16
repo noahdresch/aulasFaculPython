ce2043d6b686d0937f232345f444bb181032edaa
NoahDresch <dreschnoah@gmail.com>
2025-09-15 22:34:59 -0300

    aula python 14, media de gastos por periodo (almoco ou jantar)

--- aula14/lunchdinner.py
+++ aula14/lunchdinner.py
@@ -5,9 +5,18 @@ sns.set(style="whitegrid")
 
 df = sns.load_dataset("tips")
 
+#total de gastos por periodo (almoco ou jantar)
 plt.figure(figsize=(8, 5))
 sns.barplot(x='time', y='total_bill', data=df, estimator=sum, ci=None, palette="Set2")
 plt.xlabel('Período (Time)')
 plt.ylabel('Total de Gastos')
 plt.title('Total de Gastos por Período (Almoço ou Jantar)')
+plt.show()
+
+#media de gastos por periodo (almoco ou jantar)
+plt.figure(figsize=(8, 5))
+sns.barplot(x='time', y='total_bill', data=df)  # por padrão calcula a média
+plt.xlabel('Período (Time)')
+plt.ylabel('Média de Gastos')
+plt.title('Média de Gastos por Período (Almoço ou Jantar)')
 plt.show()
