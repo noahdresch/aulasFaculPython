fd3aa5923fba668e2f7620934a619b123357edf7
NoahDresch <dreschnoah@gmail.com>
2025-09-17 22:08:27 -0300

    aula python 18, anotacao de aula

--- aula18/anotacoesmais.py
+++ aula18/anotacoesmais.py
@@ -1,3 +1,23 @@
+
+#doctest
+def sum_numbers(numbers):
+    """
+    Soma os números em uma lista.
+
+    Exemplos:
+    >>> sum_numbers([1, 2, 3, 4])
+    10
+    >>> sum_numbers([-1, 0, 1])
+    0
+    >>> sum_numbers([])
+    0
+    """
+    return sum(numbers)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+
 #unittest
 import unittest
 def sum_numbers(numbers):
